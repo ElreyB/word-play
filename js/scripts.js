36fb0827dd62c0854a42b635d39768e322a793a9
Elrey Bellmonti and Kelsey Langlois <elrey@>
2017-08-14 13:22:02 -0700

    Add word counter.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+var getWordCounts = function(sentence) {
+  var words = sentence.reduce(function(count, word) {
+    if (count[word] > 0) {
+      count[word] += 1;
+    } else {
+      count[word] = 1;
+    }
+    return count;
+  }, {});
+  return words;
+}
+
 $(document).ready(function(){
   $("#sentence").submit(function(event){
     event.preventDefault();
@@ -9,5 +21,10 @@ $(document).ready(function(){
 
     var displaySentence = newSentence.reverse().join(" ");
     $(".result").append("<p>" + displaySentence + "</p>");
+
+    var wordCounts = getWordCounts(displaySentence.split(" "));
+    for (var word in wordCounts) {
+      $(".result").append("<p>" + word + ": " + wordCounts[word] + "</p>");
+    }
   });
 });
